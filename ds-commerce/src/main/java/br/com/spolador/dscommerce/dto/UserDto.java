9b728299f65195f5acd04d967e989cca4cf3e8ff
Luiz Henrique Fernandes Spolador <120989515+luizspolador@users.noreply.github.com>
2023-12-05 11:32:46 -0300

    obtendo usuário logado

---
+++ ds-commerce/src/main/java/br/com/spolador/dscommerce/dto/UserDto.java
@@ -0,0 +1,54 @@
+package br.com.spolador.dscommerce.dto;
+
+import br.com.spolador.dscommerce.entities.User;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDto {
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private LocalDate birthDate;
+    private List<String> roles = new ArrayList<>();
+
+    public UserDto(){}
+
+    public UserDto(User entity) {
+        id = entity.getId();
+        name = entity.getName();
+        email = entity.getEmail();
+        phone = entity.getPhone();
+        birthDate = entity.getBirthDate();
+        for(GrantedAuthority role : entity.getRoles()){
+            roles.add(role.getAuthority());
+        }
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public List<String> getRoles() {
+        return roles;
+    }
+}
--- ds-commerce/src/main/java/br/com/spolador/dscommerce/services/UserService.java
+++ ds-commerce/src/main/java/br/com/spolador/dscommerce/services/UserService.java
@@ -1,14 +1,19 @@
 package br.com.spolador.dscommerce.services;
 
+import br.com.spolador.dscommerce.dto.UserDto;
 import br.com.spolador.dscommerce.entities.Role;
 import br.com.spolador.dscommerce.entities.User;
 import br.com.spolador.dscommerce.projections.UserDetailsProjection;
 import br.com.spolador.dscommerce.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -34,5 +39,22 @@ public class UserService implements UserDetailsService {
         }
 
         return user;
+    }
+
+    protected User authenticated(){
+        try {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            Jwt jwtPrincipal = (Jwt) authentication.getPrincipal();
+            String username = jwtPrincipal.getClaim("username");
+            return repository.findByEmail(username).get();
+        } catch (Exception e){
+            throw new UsernameNotFoundException("Email not found");
+        }
+    }
+
+    @Transactional(readOnly = true)
+    public UserDto getMe(){
+        User user = authenticated();
+        return new UserDto(user);
     }
 }
